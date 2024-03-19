6c07e7fc34875238b563b6dd5b7787e43d0dd5ff
Juan Chuc <jachucme@gmail.com>
2024-03-18 23:16:03 -0600

    FEAT: API de prueba para clase UserServlet

--- src/main/java/com/example/samplejakarta/UserServlet.java
+++ src/main/java/com/example/samplejakarta/UserServlet.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import com.google.gson.Gson;
 
-@WebServlet(name = "userServlet", value = "/user-servlet")
+@WebServlet("/user-servlet/*")
 public class UserServlet extends HttpServlet {
     private Gson _gson = null;
 
@@ -31,8 +31,50 @@ public class UserServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        User user = new User("Juan", "Chuc", "jachucme");
-        sendAsJson(response, user);
+        try {
+            String[] path = request.getPathInfo().split("/");
+
+            for (String element : path) {
+                System.out.println(element);
+            }
+
+            if (request.getPathInfo().equals("/") || request.getPathInfo() == null) {
+                PrintWriter out = response.getWriter();
+                out.println("<html><body>");
+                out.println("<h1>" + "Vista de usuarios" + "</h1>");
+                out.println("</body></html>");
+                return;
+            }
+
+            if (path.length == 2) {
+                PrintWriter out = response.getWriter();
+                out.println("<html><body>");
+                out.printf("<h1>Vista de usuario %s</h1>", path[1]);
+                out.println("</body></html>");
+                return;
+            }
+
+            if (path.length == 3) {
+                if (path[2].equals("edit")) {
+                    PrintWriter out = response.getWriter();
+                    out.println("<html><body>");
+                    out.printf("<h1>Vista para editar usuario %s</h1>", path[1]);
+                    out.println("</body></html>");
+                    return;
+                }
+            }
+
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>404 NOT FOUND!</h1>");
+            out.println("</body></html>");
+        } catch (Exception e) {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + "Vista de usuarios" + "</h1>");
+            out.println("</body></html>");
+            return;
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
